0f58279c837810e2644f308f3a95b89fac4424ae
Nika Kolesnikova <firstnamenika@gmail.com>
2024-12-31 13:17:59 -0500

    feat: initial set up

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 // Application-wide dependencies
-type applicaton struct {
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
 }
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	// Error and info logs
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	// set up an application config
-	app := applicaton{}
-	// get application routes
-	router := app.routes()
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	// HTTP server config
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
 	// print out a message
-	log.Println("Starting server on port :8080 ...")
+	app.infoLog.Printf("Starting server on port %v...", *addr)
 	// start the server
-	err := http.ListenAndServe(":8080", router)
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module kweeuhree/test-go-web-app
 
 go 1.22.5
 
-require github.com/go-chi/chi/v5 v5.2.0 // indirect
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/justinas/alice v1.2.0
+)
